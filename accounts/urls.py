89424df318afffa5569817adff95d04f9778e857
shuyan <shuyanchen1999@gmail.com>
2024-06-19 10:16:58 +0200

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', login_required(CustomLoginView.as_view()), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', login_required(auth_views.LogoutView.as_view()), name='logout'),
 ]
