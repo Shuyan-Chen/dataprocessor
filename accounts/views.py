826645cb772ae010d4191237851518740cd258eb
shuyan <shuyanchen1999@gmail.com>
2024-06-19 09:39:00 +0200

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import CreateAPIView
 from django.contrib.auth.models import User
-from accounts.serializers import UserSerializer
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from accounts.serializers import UserSerializer
+from rest_framework.generics import CreateAPIView
+
 
 class RegisterAPIView(CreateAPIView):
     queryset = User.objects.all()
@@ -12,4 +14,12 @@ class RegisterAPIView(CreateAPIView):
         response = super().create(request, *args, **kwargs)
         if response.status_code == 201:
             return redirect(reverse('login'))
+        return response
+    
+class CustomLoginView(LoginView):
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.user.is_authenticated:
+            return redirect(reverse('upload'))
         return response
