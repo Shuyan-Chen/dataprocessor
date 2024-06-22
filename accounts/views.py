8a5f190fcaab5a8e483690be929a2a9cd4f17e07
shuyan <shuyanchen1999@gmail.com>
2024-06-22 10:16:25 +0200

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
@@ -29,6 +27,7 @@ class LoginView(APIView):
             password = serializer.validated_data['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
+                login(request, user)
                 return Response({
                     'message': 'Login successful',
                     'user_id': user.id,
--- dataprocessor/settings.py
+++ dataprocessor/settings.py
@@ -133,11 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10,
-# }
-
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
